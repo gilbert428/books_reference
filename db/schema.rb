bba992eac8beaadc60aa533abe8b0f54aa201ce3
Gilbert Lagman <gilbertlagmans@gmail.com>
2024-06-28 16:28:03 -0500

    seed working

---
+++ db/schema.rb
@@ -0,0 +1,74 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.1].define(version: 2024_06_28_212054) do
+  create_table "agents", force: :cascade do |t|
+    t.integer "agent_id"
+    t.string "name"
+    t.string "alias"
+    t.date "birth_date"
+    t.string "webpage"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "agents_books", force: :cascade do |t|
+    t.integer "agent_id", null: false
+    t.integer "book_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["agent_id"], name: "index_agents_books_on_agent_id"
+    t.index ["book_id"], name: "index_agents_books_on_book_id"
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.integer "book_id"
+    t.string "title"
+    t.text "description"
+    t.integer "downloads"
+    t.string "license"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books_subjects", force: :cascade do |t|
+    t.integer "book_id", null: false
+    t.integer "subject_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_books_subjects_on_book_id"
+    t.index ["subject_id"], name: "index_books_subjects_on_subject_id"
+  end
+
+  create_table "resources", force: :cascade do |t|
+    t.integer "resource_id"
+    t.string "uri"
+    t.string "resource_type"
+    t.integer "book_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_resources_on_book_id"
+  end
+
+  create_table "subjects", force: :cascade do |t|
+    t.integer "subject_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "agents_books", "agents"
+  add_foreign_key "agents_books", "books"
+  add_foreign_key "books_subjects", "books"
+  add_foreign_key "books_subjects", "subjects"
+  add_foreign_key "resources", "books"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,88 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'open-uri'
+require 'json'
+
+# URLs for the APIs
+agent_url = "https://gnikdroy.pythonanywhere.com/api/agent/"
+book_url = "https://gnikdroy.pythonanywhere.com/api/book/"
+subject_url = "https://gnikdroy.pythonanywhere.com/api/subject/"
+
+# Fetch and parse JSON data from the APIs
+agents_data = JSON.parse(URI.open(agent_url).read)
+books_data = JSON.parse(URI.open(book_url).read)
+subjects_data = JSON.parse(URI.open(subject_url).read)
+
+
+# Seed Subjects
+
+  subjects_data['results'].each do |subject|
+    next if subjects_data['next'].nil?
+    Subject.find_or_create_by!(
+      subject_id: subject['id'],
+      name: subject['name']
+    )
+  end
+
+
+
+# Seed Agents
+agents_data['results'].each do |agent|
+  next if agent['id'].nil?
+
+  new_agent = Agent.find_or_create_by!(
+    agent_id: agent['id'],
+    name: agent['person']['name'],
+    alias: agent['person']['alias'],
+    birth_date: agent['person']['birth_date'],
+    webpage: agent['person']['webpage']
+  )
+
+  if new_agent.persisted?
+    # Seed Books and Resources
+    books_data['results'].each do |book|
+      new_book = Book.find_or_create_by!(
+        book_id: book['id'],
+        title: book['title'],
+        description: book['description'],
+        downloads: book['downloads'],
+        license: book['license']
+      )
+
+      # Seed Resources
+      book['resources'].each do |resource|
+        Resource.find_or_create_by!(
+          resource_id: resource['id'],
+          book: new_book,
+          uri: resource['uri'],
+          resource_type: resource['type']
+        )
+      end
+
+      # Seed Book-Agent Associations
+      book['agents'].each do |agent|
+        if agent['id'] == new_agent.agent_id
+          AgentsBook.find_or_create_by!(
+            agent: new_agent,
+            book: new_book
+          )
+        end
+      end
+
+      # Seed Book-Subject Associations
+      book['subjects'].each do |subject|
+        subject_record = Subject.find_by(name: subject)
+        if subject_record
+          BooksSubject.find_or_create_by!(
+            book: new_book,
+            subject: subject_record
+          )
+        else
+          puts "Subject #{subject} not found"
+        end
+      end
+
+    end
+  end
+end
