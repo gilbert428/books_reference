6f75c34d09580417dbe1e9bde35e5516de89d011
Gilbert Lagman <gilbertlagmans@gmail.com>
2024-07-03 09:27:44 -0500

    added image function

--- app/admin/book.rb
+++ app/admin/book.rb
@@ -1,5 +1,6 @@
+# app/admin/books.rb
 ActiveAdmin.register Book do
-  permit_params :title, :description, :downloads, :license, agent_ids: [], subject_ids: []
+  permit_params :title, :description, :downloads, :license, :image, agent_ids: [], subject_ids: []
 
   form do |f|
     f.inputs 'Book Details' do
@@ -9,7 +10,62 @@ ActiveAdmin.register Book do
       f.input :license
       f.input :agents, as: :check_boxes
       f.input :subjects, as: :check_boxes
+      f.input :image, as: :file
     end
     f.actions
   end
+
+  show do
+    attributes_table do
+      row :title
+      row :description
+      row :downloads
+      row :license
+      row :agents do |book|
+        book.agents.map(&:name).join(', ')
+      end
+      row :subjects do |book|
+        book.subjects.map(&:name).join(', ')
+      end
+      row :image do |book|
+        if book.image.attached?
+          image_tag url_for(book.image)
+        else
+          content_tag(:span, "No image")
+        end
+      end
+    end
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :title
+    column :description
+    column :downloads
+    column :license
+    column :agents do |book|
+      book.agents.map(&:name).join(', ')
+    end
+    column :subjects do |book|
+      book.subjects.map(&:name).join(', ')
+    end
+    column :image do |book|
+      if book.image.attached?
+        image_tag url_for(book.image), size: "50x50"
+      else
+        content_tag(:span, "No image")
+      end
+    end
+    actions
+  end
+
+  filter :title
+  filter :description
+  filter :downloads
+  filter :license
+  filter :agents
+  filter :subjects
+  filter :created_at
+  filter :updated_at
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,9 +2,10 @@ class Book < ApplicationRecord
   has_and_belongs_to_many :agents, join_table: :agents_books
   has_and_belongs_to_many :subjects, join_table: :books_subjects
   has_many :resources
+  has_one_attached :image
 
   def self.ransackable_attributes(auth_object = nil)
-    ["book_id", "created_at", "description", "downloads", "id", "id_value", "license", "title", "updated_at"]
+    ["title", "description", "downloads", "license", "created_at", "updated_at"]
   end
 
   def self.ransackable_associations(auth_object = nil)
@@ -13,6 +14,4 @@ class Book < ApplicationRecord
 
   validates :title, presence: true
   validates :downloads, numericality: { only_integer: true }
-
-
 end
