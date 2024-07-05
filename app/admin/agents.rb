f9ad55bfa8cb778bb5b63f3d35189c3ff20b99af
Gilbert Lagman <gilbertlagmans@gmail.com>
2024-07-04 23:07:10 -0500

    fixed my books index

--- app/admin/agents.rb
+++ app/admin/agents.rb
@@ -4,8 +4,12 @@ ActiveAdmin.register Agent do
   form do |f|
     f.inputs 'Agent Details' do
       f.input :name
-      f.input :books, as: :check_boxes
+      f.input :books, as: :check_boxes, collection: Book.all
     end
     f.actions
   end
+
+  # Add filters for searching and filtering agents
+  filter :name
+  filter :books_id_eq, as: :select, collection: -> { Book.all.pluck(:title, :id) }
 end
--- app/models/agent.rb
+++ app/models/agent.rb
@@ -2,7 +2,7 @@ class Agent < ApplicationRecord
   has_and_belongs_to_many :books, join_table: :agents_books
 
   def self.ransackable_associations(auth_object = nil)
-    ["books"]
+     ["books"]
   end
 
   def self.ransackable_attributes(auth_object = nil)
