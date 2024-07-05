65e809be61b575be341864449a0994c5b8ec4893
Gilbert Lagman <gilbertlagmans@gmail.com>
2024-07-05 10:29:17 -0500

    admin/subject  page working

--- app/admin/subject.rb
+++ app/admin/subject.rb
@@ -4,8 +4,32 @@ ActiveAdmin.register Subject do
   form do |f|
     f.inputs 'Subject Details' do
       f.input :name
-      f.input :books, as: :check_boxes
+      f.input :books, as: :check_boxes, collection: Book.all
     end
     f.actions
   end
+
+  show do
+    attributes_table do
+      row :name
+      row :books do |subject|
+        subject.books.map(&:title).join(', ')
+      end
+    end
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :books do |subject|
+      subject.books.map(&:title).join(', ')
+    end
+    actions
+  end
+
+  filter :name
+  filter :books_title_cont, as: :string, label: 'Book Title'
+  filter :created_at
+  filter :updated_at
 end
